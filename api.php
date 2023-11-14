2d31380c26d77e4f7ddf44f2d396dc1a5879d6c5
inteligenciafutura <inteligenciafuturatv@gmail.com>
2023-11-14 18:16:33 -0500

    ultima implementacion de seguridad

--- api.php
+++ api.php
@@ -12,43 +12,71 @@ $dea = new Dea($db);
 
 $method = $_SERVER['REQUEST_METHOD'];
 
-switch ($method) {
-    case 'GET':
-        $id = $_GET['id'] ?? null;
-        $result = $dea->read($id);
-        echo json_encode($result);
-        break;
+try {
+    switch ($method) {
+        case 'GET':
+            $id = $_GET['id'] ?? null;
+            $result = $dea->read($id);
+            echo json_encode($result);
+            break;
 
-    case 'POST':
-        $data = json_decode(file_get_contents('php://input'), true);
-        if ($dea->create($data)) {
-            echo json_encode(['message' => 'dea creado']);
-        } else {
-            echo json_encode(['message' => 'Error al crear dea']);
-        }
-        break;
+        case 'POST':
+            $data = json_decode(file_get_contents('php://input'), true);
 
-    case 'PUT':
-        $data = json_decode(file_get_contents('php://input'), true);
-        if ($dea->update($data)) {
-            echo json_encode(['message' => 'dea actualizado']);
-        } else {
-            echo json_encode(['message' => 'Error al actualizar dea']);
-        }
-        break;
+            // Validación genérica
+            if (empty($data)) {
+                http_response_code(400); // Respuesta de error Bad Request
+                echo json_encode(['message' => 'Los datos de entrada están vacíos o no son válidos.']);
+            } else {
+                // Los datos son válidos, continúa con el procesamiento.
+                if ($dea->create($data)) {
+                    echo json_encode(['message' => 'dea creado']);
+                } else {
+                    throw new Exception('Error al crear dea');
+                }
+            }
+            break;
 
-    case 'DELETE':
-        $id = $_GET['id'];
-        if ($dea->delete($id)) {
-            echo json_encode(['message' => 'dea eliminado']);
-        } else {
-            echo json_encode(['message' => 'Error al eliminar dea']);
-        }
-        break;
+        case 'PUT':
+            $data = json_decode(file_get_contents('php://input'), true);
 
-    default:
-        http_response_code(405);
-        echo json_encode(['message' => 'Método no permitido']);
-        break;
+            // Validación genérica
+            if (empty($data)) {
+                http_response_code(400); // Respuesta de error Bad Request
+                echo json_encode(['message' => 'Los datos de entrada están vacíos o no son válidos.']);
+            } else {
+                // Los datos son válidos, continúa con el procesamiento.
+                if ($dea->update($data)) {
+                    echo json_encode(['message' => 'dea actualizado']);
+                } else {
+                    throw new Exception('Error al actualizar dea');
+                }
+            }
+            break;
+
+        case 'DELETE':
+            $id = $_GET['id'];
+            if ($dea->delete($id)) {
+                echo json_encode(['message' => 'dea eliminado']);
+            } else {
+                throw new Exception('Error al eliminar dea');
+            }
+            break;
+
+        default:
+            http_response_code(405);
+            echo json_encode(['message' => 'Método no permitido']);
+            break;
+    }
+} catch (PDOException $e) {
+    // Error de base de datos
+    error_log('Error de base de datos: ' . $e->getMessage());
+    http_response_code(500);
+    echo json_encode(['message' => 'Error interno de la base de datos.']);
+} catch (Exception $e) {
+    // Otros errores
+    error_log('Error: ' . $e->getMessage());
+    http_response_code(500);
+    echo json_encode(['message' => 'Ha ocurrido un error en el servidor.']);
 }
 ?>
